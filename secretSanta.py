17bb2ff0ef67c4cc839c34afa11a91f2b960e1aa
Rafael Valdez <valdezr1@outlook.com>
2017-11-22 17:47:19 -0800

    randomiz function initialized

--- secretSanta.py
+++ secretSanta.py
@@ -1,3 +1,15 @@
 # Andrew Ang & Rafael Valdez & Henry Ang
 
 print("Hello there!")
+
+
+import random
+
+# Description: Takes in list of people and will randomize order and assign secret santa
+# Parameters: List of people's names and dictionary
+# Returns: Dictionary of people
+def randomize(nameList, assignDictionary):
+    # Randomizing List
+  
+
+    print("Garbage")
