fa75029ab2369b0446f1abfcc9a870f310ee88e5
Rafael Valdez <valdezr1@outlook.com>
2017-11-24 18:16:14 -0800

    noIsland Function working and printed to console

--- secretSanta.py
+++ secretSanta.py
@@ -1,15 +1,35 @@
 # Andrew Ang & Rafael Valdez & Henry Ang
-
-print("Hello there!")
-
-
 import random
+
 
 # Description: Takes in list of people and will randomize order and assign secret santa
 # Parameters: List of people's names and dictionary
 # Returns: Dictionary of people
-def randomize(nameList, assignDictionary):
+def noIsland(nameList, assignDictionary):
     # Randomizing List
-  
+    random.shuffle(nameList)
+    for x in range(0, len(nameList)):
+        if x == len(nameList) - 1:
+            assignDictionary[nameList[x]] = nameList[0]
+            print(nameList[x] + " : " + nameList[0])
+        else:
+            assignDictionary[nameList[x]] = nameList[x+1]
+            print(nameList[x] + " : " + nameList[x+1])
 
-    print("Garbage")
+    # print(assignDictionary)
+
+myList = ["Naveen", "Courtney", "Robert", "Hector", "Andrew", "Henry", "Rafael"]
+myDict = {
+    "Naveen": "",
+    "Courtney": "",
+    "Robert": "",
+    "Hector": "",
+    "Andrew": "",
+    "Henry": "",
+    "Rafael": ""
+}
+
+noIsland(myList, myDict)
+    # [Naveen , Courtney, Robert]
+
+    # Naveen : Courtney
